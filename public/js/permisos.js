507bcf655d88c38fd816b09f2f139a18525f3a50
bernyT <btroffer@gmail.com>
2015-03-17 00:43:30 -0300

    Se agrego permisos.js

---
+++ public/js/permisos.js
@@ -0,0 +1,12 @@
+$(function () {
+	/*$("#tablaArchivos tbody tr butto").on("click", function(){
+		console.log( $( this ).parent() );
+	});*/
+
+	$("#tablaArchivos tbody tr").each( function(){
+		$(this button).on( "click",funtion(){
+			console.log( $( this ) );
+		});
+		
+	});
+});
